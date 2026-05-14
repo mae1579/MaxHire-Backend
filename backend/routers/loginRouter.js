218cf89936933277aefd45434d532ff46dc8ddf5
mae1579 <Kompot6448231!>
2026-05-14 12:44:26 +0200

    Dodano alternatywne logowanie za pomoca adresu e-mail, poprawiono bug w przypadku funkcjonalosci resetu hasla

--- backend/routers/loginRouter.js
+++ backend/routers/loginRouter.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { UserRecord } = require("../records/UserRecord");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const {createMailOptions} = require("../utils/szablonPassword");
+const {transporter} = require("../utils/gmailconfig");
+const {ValidationError} = require("../utils/handleErrors");
+const {loginByEmail} = require("../utils/szablonLoginUserByEmail");
 
 const loginRouter = express.Router();
 
@@ -23,13 +27,13 @@ loginRouter.post("/", async (req, res, next) => {
 
   const tokenPayload = { id: user.id, role: user.role };
   const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "1d",
   });
 
   res
     .cookie("token_auth", token, {
       httpOnly: true,
-      maxAge: 2592000,
+      maxAge: 24 * 60 * 60 * 1000,
       sameSite: "strict",
     })
 
@@ -41,7 +45,63 @@ loginRouter.post("/", async (req, res, next) => {
 });
 
 
-//Logowanie alternatynwe za pomocą adres e-mail.
+//Logowanie alternatynwe za pomocą adres e-mail. Etap 1
+loginRouter.post("/email/loginuser", async(req,res) => {
+  const loginUser = req.body.email;
+  const findUser = await UserRecord.findOneByEmail(loginUser);
+  if(!findUser){
+    return res.status(200).json({Message:"Konto uzytkownika o takim e-mailu nie istnieje"});
+  }
+  const tokenPayload = { id: findUser.id, role: findUser.role };
+  const token = jwt.sign(tokenPayload, process.env.JWT_EMAILLOGIN, {
+    expiresIn: "60s",
+  });
+
+  const link = `http://localhost:5173/login/${token}`;
+
+  const mailbody = loginByEmail(findUser.email, link, findUser.name);
+
+  try {
+    await transporter.sendMail(mailbody);
+    res
+        .status(200)
+        .json({ message: "Wiadomosc z linkiem do logowania zostala wyslana" });
+  } catch (error) {
+    throw new ValidationError(
+        "Wiadomosc nie moze zostac wyslana, sprobuj ponownie pozniej",
+    );
+  }
+
+});
+
+loginRouter.post("/email/login/", async(req,res) => {
+  const tokenUser = req.body.token;
+  let token;
+  try {
+    token = jwt.verify(tokenUser, process.env.JWT_EMAILLOGIN);
+  }catch(err){
+    return res.status(403).json({message:"Link wygasl !"});
+  }
+
+  const findUser = await UserRecord.findById(token.id);
+  const payload = { id: findUser.id, role: findUser.role };
+  const tokenLoginUser = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res
+      .cookie("token_auth", tokenLoginUser, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "strict",
+      })
+
+      .status(200)
+      .json({
+        message: "Zalogowano pomyślnie",
+        user: { id: findUser.id, email: findUser.email, role: findUser.role, name: findUser.name, surname: findUser.surname, photo: findUser.photo, phone: findUser.phone },
+      });
+});
 
 
 module.exports = {
---
+++ backend/utils/szablonLoginUserByEmail.js
@@ -0,0 +1,53 @@
+const { transporter } = require("./gmailconfig");
+const
+    loginByEmail = (userEmail, link, userName) => {
+        return {
+            from: process.env.GMAIL_APP_EMAIL,
+            to: userEmail,
+            subject: `Logowanie konta`,
+            link: "",
+            html: `
+    <div style="background-color: #000000; color: #ffffff; padding: 40px; font-family: 'Segoe UI', Arial, sans-serif; text-align: center;">
+        
+        <h2 style="color: #ffffff; margin-bottom: 20px;">Logowanie do konta ${userName}</h2>
+        
+        <p style="font-size: 16px; color: #dddddd; line-height: 1.6;">
+            Otrzymaliśmy prośbę o zalogowanie do Twojego konta  w systemie, kliknij w link aby sie zalogowac <strong>MaxHire</strong>.
+        </p>
+
+        <div style="margin: 40px 0;">
+            <a href="${link}"
+               style="background-color: #ffffff; color: #000000; padding: 14px 30px; text-decoration: none; font-weight: bold; border-radius: 4px; display: inline-block;">
+               ZALOGUJ SIE
+            </a>
+        </div>
+
+        <p style="font-size: 13px; color: #888888;">
+            Jeśli to nie Ty wysłałeś zapytanie, po prostu zignoruj tę wiadomość.
+        </p>
+
+        <div style="margin-top: 50px; padding-top: 20px; border-top: 1px solid #333333; text-align: center;">
+            <img src="https://res.cloudinary.com/dp8o4hppt/image/upload/v1768140555/MaxHire_m1ojpo.png" 
+                 alt="MaxHire Footer"
+                 style="width: 100%; max-width: 600px; height: auto; display: block; margin: 0 auto; border: 0;">
+        </div>
+    </div>
+    `,
+        };
+    };
+
+//Uzycie wysylki.
+// const mailbody = {
+//   ...mailOptions,
+//   to: "mateuszumiecki@gmail.com",
+// };
+// try {
+//   await transporter.sendMail(mailbody);
+//   res.send("Poszło! Sprawdź skrzynkę.");
+// } catch (error) {
+//   res.status(500).send("Coś padło: " + error.message);
+// }
+
+module.exports = {
+    loginByEmail,
+};
